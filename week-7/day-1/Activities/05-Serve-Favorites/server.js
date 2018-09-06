d25509b7cc1140ef834160f9c64e658236e6621e
bobbiro2e <bobbiro2e@gmail.com>
2018-09-06 11:11:39 -0500

    week7day2

---
+++ week-7/day-1/Activities/05-Serve-Favorites/server.js
@@ -0,0 +1,74 @@
+var http = require("http");
+var fs = require("fs");
+
+var PORT = 8080;
+
+var server = http.createServer(handleRequest);
+
+server.listen(PORT, function () {
+    console.log("Server listening on: http://localhost:" + PORT);
+});
+
+function handleRequest(req, res) {
+
+    var path = req.url;
+
+    switch (path) {
+
+        case "/":
+            return displayRoot(path, req, res);
+
+        case "/foods":
+            return displayFoods(path, req, res);
+
+        case "/movies":
+            return displayMovies(path, req, res);
+
+        case "/favcss":
+            return displayFavcss(path, req, res);
+
+        default:
+            return display404(path, req, res);
+    }
+}
+
+function displayRoot(url, req, res) {
+    fs.readFile(__dirname + "/index.html", function (err, data) {
+
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.end(data);
+    });
+}
+
+function displayFoods(url, req, res) {
+    fs.readFile(__dirname + "/foods.html", function (err, data) {
+
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.end(data);
+    });
+}
+function displayMovies(url, req, res) {
+    fs.readFile(__dirname + "/movies.html", function (err, data) {
+
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.end(data);
+    });
+}
+function displayFavcss(url, req, res) {
+    fs.readFile(__dirname + "/favcss.html", function (err, data) {
+
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.end(data);
+    });
+}
+
+function display404(url, req, res) {
+    var myHTML = "<html>" +
+        "<body><h1>404 Not Found </h1>" +
+        "<p>The page you were looking for: " + url + " can not be found</p>" +
+        "</body></html>";
+
+    res.writeHead(404, { "Content-Type": "text/html" });
+
+    res.end(myHTML);
+}
