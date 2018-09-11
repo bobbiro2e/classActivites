139946d55755d66e02df19b36fd23e9d54c28d55
bobbiro2e <bobbiro2e@gmail.com>
2018-09-11 12:33:42 -0500

    week8day1

--- week-7/day-4/Activities/05-BenAndJerrys/server.js
+++ week-7/day-4/Activities/05-BenAndJerrys/server.js
@@ -1,15 +1,10 @@
-// Dependencies
 var express = require("express");
 var exphbs = require("express-handlebars");
 
-// Create an instance of the express app.
 var app = express();
 
-// Set the port of our application
-// process.env.PORT lets the port be set by Heroku
 var PORT = process.env.PORT || 8080;
 
-// Set Handlebars as the default templating engine.
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
@@ -30,13 +25,9 @@ app.get("/icecream/:name", function (req, res) {
 });
 
 app.get("/icecreams", function (req, res) {
-    res.render("all-flavors", {
-        icecreams
-    })
+    res.render("all-flavors", { icecreams })
 });
 
-
 app.listen(PORT, function () {
-    // Log (server-side) when our server has started
     console.log("Server listening on: http://localhost:" + PORT);
 });
