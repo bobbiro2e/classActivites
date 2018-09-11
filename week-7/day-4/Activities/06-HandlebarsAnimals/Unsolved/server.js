840a31a0282a0a8142be94d8c91cb68c8bbf2472
bobbiro2e <bobbiro2e@gmail.com>
2018-09-11 12:40:37 -0500

    week8day1

--- week-7/day-4/Activities/06-HandlebarsAnimals/Unsolved/server.js
+++ week-7/day-4/Activities/06-HandlebarsAnimals/Unsolved/server.js
@@ -1,8 +1,6 @@
 var express = require("express");
 var app = express();
 
-// Set the port of our application
-// process.env.PORT lets the port be set by Heroku
 var PORT = process.env.PORT || 8080;
 
 var exphbs = require("express-handlebars");
