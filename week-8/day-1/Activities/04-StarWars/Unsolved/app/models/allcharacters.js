bf735ef2dc3f8ba8b29a86898c4fa99dfd7e3ac9
bobbiro2e <bobbiro2e@gmail.com>
2018-09-17 11:12:13 -0500

    week9day1

---
+++ week-8/day-1/Activities/04-StarWars/Unsolved/app/models/allcharacters.js
@@ -0,0 +1,20 @@
+// Dependencies
+// =============================================================
+
+// This may be confusing but here Sequelize (capital) references the standard library
+var Sequelize = require("sequelize");
+// sequelize (lowercase) references our connection to the DB.
+var sequelize = require("../config/connection.js");
+
+var AllCharacters = sequelize.define("all-characters", {
+  routeName: Sequelize.STRING,
+  name: Sequelize.STRING,
+  role: Sequelize.STRING,
+  age: Sequelize.INTEGER,
+  forcePoints: Sequelize.INTEGER
+});
+
+// Syncs with DB
+AllCharacters.sync();
+
+module.exports = AllCharacters;
--- week-8/day-1/Activities/04-StarWars/Unsolved/schema.sql
+++ week-8/day-1/Activities/04-StarWars/Unsolved/schema.sql
@@ -6,17 +6,6 @@ It is important since other developers will be unlikely to have the same databas
 /* Create and use the starwars db */
 DROP DATABASE IF EXISTS `starwars`;
 CREATE DATABASE `starwars`;
-USE `starwars`;
 
 
-/* Create a table for all your star wars characters */
-CREATE TABLE `allcharacters` (
-	`id` Int( 11 ) AUTO_INCREMENT NOT NULL,
-	`routeName` VARCHAR( 255) NOT NULL,
-	`name` VARCHAR( 255 ) NOT NULL,
-	`role` VARCHAR( 255 ) NOT NULL,
-	`age` Int(11) NOT NULL,
-	`forcePoints` Int(11) NOT NULL,
-	/* Set ID as primary key */
-	PRIMARY KEY ( `id` )
-);
+
