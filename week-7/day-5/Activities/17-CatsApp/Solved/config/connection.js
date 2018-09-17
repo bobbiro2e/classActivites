f3bb7b5891aa65b8710c3ae9a418019e1e530670
bobbiro2e <bobbiro2e@gmail.com>
2018-09-17 10:19:57 -0500

    week9day1

--- week-7/day-5/Activities/17-CatsApp/Solved/config/connection.js
+++ week-7/day-5/Activities/17-CatsApp/Solved/config/connection.js
@@ -1,16 +1,21 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
+var connection;
+
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
   user: "root",
-  password: "",
-  database: "cat_db"
+  password: "root",
+  database: "cat_db",
+  socketPath: "/Applications/MAMP/tmp/mysql/mysql.sock"
 });
 
 // Make connection.
-connection.connect(function(err) {
+connection.connect(function (err) {
   if (err) {
     console.error("error connecting: " + err.stack);
     return;
