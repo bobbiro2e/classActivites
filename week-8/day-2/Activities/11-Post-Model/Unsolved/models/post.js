79f937e7de98439f086915ac1c8b372f133252b5
bobbiro2e <bobbiro2e@gmail.com>
2018-09-18 14:20:04 -0500

    week9day2

--- week-8/day-2/Activities/11-Post-Model/Unsolved/models/post.js
+++ week-8/day-2/Activities/11-Post-Model/Unsolved/models/post.js
@@ -1,5 +1,17 @@
-module.exports = function(sequelize, DataTypes) {
-  // Add code here to create a Post model
-  // This model needs a title, a body, and a category
-  // Don't forget to 'return' the post after defining
+module.exports = function (sequelize, DataTypes) {
+  var Post = sequelize.define("post", {
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    body: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    category: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
+  });
+  return Post;
 };
